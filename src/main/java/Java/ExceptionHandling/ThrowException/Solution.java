8df4cb8447b28a6ac82f1bb1a8995a4e51bb9382
khiru <khirulislam.cse@gmail.com>
2017-07-26 18:04:12 +0600

    throwing exception from method

---
+++ src/main/java/Java/ExceptionHandling/ThrowException/Solution.java
@@ -0,0 +1,36 @@
+package Java.ExceptionHandling.ThrowException;
+
+import java.util.*;
+import java.util.Scanner;
+
+/**
+ * Created by khirul
+ * on 7/26/2017.
+ */
+
+class MyCalculator {
+    public int power(double n, double p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
+        }
+        return (int) Math.pow(n, p);
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            MyCalculator my_calculator = new MyCalculator();
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
