9de373387b3148880e6e65b5ab7d376f48d32430
khiru <khirulislam.cse@gmail.com>
2017-07-27 16:31:46 +0600

    Java generic example

---
+++ src/main/java/Java/DataStracture/JavaGenerics/Solution.java
@@ -0,0 +1,40 @@
+package Java.DataStracture.JavaGenerics;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by khirul
+ * on 7/27/2017.
+ */
+
+class Printer<T> {
+    public void printArray(T[] values) {
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(values[i]);
+        }
+    }
+}
+
+public class Solution {
+
+
+    public static void main(String args[]) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if (name.equals("printArray"))
+                count++;
+        }
+
+        if (count > 1) System.out.println("Method overloading is not allowed!");
+
+    }
+}
