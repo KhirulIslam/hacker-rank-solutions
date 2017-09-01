ea1f148443e54db2346f82d5ab1ced414ecad2dd
KhirulIslam <khirulislam.cse@gmail.com>
2017-09-01 22:09:12 +0600

    Solution for problem : Java Loops II

---
+++ src/main/java/Java/Introduction/JavaLoopTwo/Solution.java
@@ -0,0 +1,25 @@
+package Java.Introduction.JavaLoopTwo;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int temp = a;
+
+            for (int j = 0; j < n; j++) {
+                temp += (int) Math.pow(2, j) * b;
+                System.out.print(temp + " ");
+            }
+
+            System.out.println();
+        }
+        in.close();
+    }
+}
