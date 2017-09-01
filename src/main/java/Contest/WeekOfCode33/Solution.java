18b44b000b21cd668c97e6e25a8ff3f5bb1412fc
KhirulIslam <khirulislam.cse@gmail.com>
2017-09-02 02:46:03 +0600

    Solution for problem : pattern count

---
+++ src/main/java/Contest/WeekOfCode33/Solution.java
@@ -0,0 +1,33 @@
+package Contest.WeekOfCode33;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    static int patternCount(String s){
+        String pattern="(?<=1)[0]+(?=1)";
+        int count = 0;
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(s);
+        while (m.find()){
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            int result = patternCount(s);
+            System.out.println(result);
+        }
+    }
+}
